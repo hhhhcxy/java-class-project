482da95b3c2af1b2f1e2b2ef447006a0729f39e2
hhhhqwq <2353912846@qq.com>
2024-11-19 10:51:58 +0800

    study2

--- src/enter/Main.java
+++ src/enter/Main.java
@@ -1,8 +1,7 @@
 package enter;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +22,30 @@ public class Main {
                 frame.dispose();
             }
         });
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode()==KeyEvent.VK_SPACE){
+                    System.out.println("jump");
+                }
+                if(e.getKeyCode()==KeyEvent.VK_SHIFT){
+                    System.out.println("run");
+                }
+            }
+        });
+        frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(e.getX()+" "+e.getY()+" "+e.getClickCount()+" "+e.getButton());
+            }
+        });
+
+        frame.setLayout(null);
+        Label label=new Label("qwq");
+        label.setLocation(100,100);
+        label.setSize(50,100);
+        label.setBackground(Color.BLACK);
+        label.setForeground(Color.WHITE);
+        frame.add(label);
     }
 }
